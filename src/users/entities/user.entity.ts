d4864f73bf96956b6f900788741e9212ab487414
shiliangL <1462999406@qq.com>
2023-09-10 09:44:26 +0800

    2023年9月10日+

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+    comment: '用户名',
+  })
+  username: string;
+
+  @Column({
+    length: 50,
+    comment: '密码',
+  })
+  password: string;
+
+  @CreateDateColumn({
+    comment: '创建时间',
+  })
+  create_time: Date;
+
+  @UpdateDateColumn({
+    comment: '更新时间',
+  })
+  update_time: Date;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    return await this.repository.findOne(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async login(createUserDto: CreateUserDto) {
+    return await this.repository.save(createUserDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    // delete createUserDto.id;
+    return await this.repository.save(createUserDto);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findAll(): Promise<UserEntity[]> {
+    return await this.repository.find();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(id: number): Promise<UserEntity> {
+    return await this.repository.findOne({ where: { id: id } });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.repository.update(id, updateUserDto);
+  }
+
+  async remove(id: number) {
+    return await this.repository.delete(id);
   }
 }
