85be8f9fe006991ecefaf7d3f2c3f73167e3c248
shiliangL <1462999406@qq.com>
2023-09-15 10:35:28 +0800

    2023年9月15日

--- src/article/dto/create-article.dto.ts
+++ src/article/dto/create-article.dto.ts
@@ -1,1 +1,6 @@
-export class CreateArticleDto {}
+export class CreateArticleDto {
+  readonly title: string;
+  readonly description: string;
+  readonly body: string;
+  readonly tagList: string[];
+}
